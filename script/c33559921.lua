7dbe00592b78015ce5b2b3c61848b2366be7bfbf
GameMaster719 <cobaltss87@yahoo.com>
2018-04-16 07:39:10 -0600

    Add files via upload

--- script/c33559921.lua
+++ script/c33559921.lua
@@ -1,21 +1,21 @@
 --Raigeki (DOR)
 --scripted by GameMaster(GM)
-function c511005783.initial_effect(c)
+function c33559921.initial_effect(c)
 --Activate
 local e1=Effect.CreateEffect(c)
 e1:SetCategory(CATEGORY_DESTROY)
 e1:SetType(EFFECT_TYPE_ACTIVATE)
 e1:SetCode(EVENT_FREE_CHAIN)
-e1:SetTarget(c511005783.target)
-e1:SetOperation(c511005783.activate)
+e1:SetTarget(c33559921.target)
+e1:SetOperation(c33559921.activate)
 c:RegisterEffect(e1)
 end
 
-function c511005783.filter(c)
+function c33559921.filter(c)
 return c:IsType(TYPE_MONSTER)
 end
 
-function c511005783.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c33559921.target(e,tp,eg,ep,ev,re,r,rp,chk)
 local c=e:GetHandler()
 local seq=e:GetHandler():GetSequence()
 local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,4-seq)
@@ -26,7 +26,7 @@ if tc then g:AddCard(tc) end
 Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
 end
 
-function c511005783.activate(e,tp,eg,ep,ev,re,r,rp)
+function c33559921.activate(e,tp,eg,ep,ev,re,r,rp)
 local seq=e:GetHandler():GetSequence()
  local g=Group.CreateGroup()
  local tc=Duel.GetFieldCard(1-tp,LOCATION_MZONE,4-seq)
