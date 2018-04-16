c03a6142a6e59b7736b7890e0bb739102a260f3f
GameMaster719 <cobaltss87@yahoo.com>
2018-04-16 00:55:19 -0600

    Add files via upload

--- script/c39573866.lua
+++ script/c39573866.lua
@@ -1,41 +1,49 @@
 --Cute Dark Knight
-local id,cod=39573866,c39573866
-function cod.initial_effect(c)
+function c39573866.initial_effect(c)
 	--Named
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_ADD_CODE)
 	e1:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
 	e1:SetValue(92216322)
 	c:RegisterEffect(e1)
 	--Equip
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCategory(CATEGORY_EQUIP)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetTarget(cod.eqtg)
-	e2:SetOperation(cod.eqop)
+	e2:SetTarget(c39573866.eqtg)
+	e2:SetOperation(c39573866.eqop)
 	c:RegisterEffect(e2)
 end
-function cod.filter(c)
+function c39573866.filter(c)
 	return c:IsType(TYPE_MONSTER)
 end
-function cod.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and cod.filter(chkc) end
+function c39573866.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c39573866.filter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(cod.filter,tp,LOCATION_GRAVE,0,1,nil) end
+		and Duel.IsExistingTarget(c39573866.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,cod.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SelectTarget(tp,c39573866.filter,tp,LOCATION_GRAVE,0,1,1,nil)
 end
-function cod.eqop(e,tp,eg,ep,ev,re,r,rp)
+function c39573866.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsFaceup() and c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) then
 		Duel.Equip(tp,tc,c,true)
+	local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		e1:SetValue(c39573866.eqlimit)
+		tc:RegisterEffect(e1)
 	end
+end
+function c39573866.eqlimit(e,c)
+	return e:GetOwner()==c
 end
