d7a8d00d205c9e35df08de3fa84d538210fdac1c
GameMaster719 <cobaltss87@yahoo.com>
2018-04-16 08:45:43 -0600

    Add files via upload

--- script/c511005768.lua
+++ script/c511005768.lua
@@ -21,7 +21,7 @@ function c511005768.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 if chkc then return chkc:IsOnField() and c511005768.filter(chkc) and chkc~=e:GetHandler() end
 if chk==0 then return Duel.IsExistingTarget(c511005768.filter,tp,0,LOCATION_ONFIELD,1,e:GetHandler(),e) 
 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(511005768,0))
 local g=Duel.SelectTarget(tp,c511005768.filter,tp,0,LOCATION_ONFIELD,1,1,e:GetHandler(),e)
 Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
 end
